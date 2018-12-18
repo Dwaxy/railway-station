dc0acbb3763a0fc607cf4b39a154cf7d5072129f
Dwaxy <joel.innes@gmail.com>
2018-12-19 12:50:13 +1300

    Update map.js

--- src/components/map.js
+++ src/components/map.js
@@ -4,7 +4,7 @@ const Map = () => {
   return (
     <section id="map">
       <div className="map-holder">
-         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDO0KrL5-vOaHzB3ig-keYi-_5buPjJmws&callback=initMap"
+         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7Orx1FRQa9ewpuSP8uOcZU6eEL6m-ziU&callback=initMap"
           type="text/javascript"></script>
       </div>
     </section>
