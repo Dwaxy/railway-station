adc849a9ada01bcfa75d51e686fa1cfda8b944c6
Dwaxy <joel.innes@gmail.com>
2018-12-19 15:14:45 +1300

    Update map.js

--- src/components/map.js
+++ src/components/map.js
@@ -11,8 +11,9 @@ src="https://www.google.com/maps/embed/v1/place?q=springfield+railway+station&ke
       src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d637.0920213525307!2d171.93047469345296!3d-43.334088098712925!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x31e01cecb6a1ce84!2sStation73+Cafe!5e0!3m2!1sen!2snz!4v1545184762983" 
       width="100%" 
       height="100%" 
-      style={{position: "absolute", border: "none"}}
-      ></iframe>
+      style={{position: "absolute", border: "none"}}> </iframe>
+      
+    </section>
   )
 }
 
