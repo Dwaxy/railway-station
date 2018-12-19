3be97df07c47091e0d9bd2e2f14a2cda965a43c0
Dwaxy <joel.innes@gmail.com>
2018-12-19 13:03:34 +1300

    Update map.js

--- src/components/map.js
+++ src/components/map.js
@@ -3,10 +3,8 @@ import React from 'react';
 const Map = () => {
   return (
     <section id="map">
-      <div className="map-holder">
-         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7Orx1FRQa9ewpuSP8uOcZU6eEL6m-ziU&callback=initMap"
-          type="text/javascript"></script>
-      </div>
+    <iframe width="600" height="450" 
+src="https://www.google.com/maps/embed/v1/place?q=springfield+railway+station&key=AIzaSyD7Orx1FRQa9ewpuSP8uOcZU6eEL6m-ziU"></iframe> 
     </section>
   )
 }
