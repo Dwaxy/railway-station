23e4bf62b42b88335814b3e6f64ccc6abfbd4119
Dwaxy <joel.innes@gmail.com>
2018-12-19 14:52:58 +1300

    Update map.js

--- src/components/map.js
+++ src/components/map.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Map = () => {
   return (
     <section id="map">
-    <iframe width="100%" height="100%" style={{position: "absolute"}}
+    <iframe width="100%" height="100%" style={{position: "absolute", border: "none"}}
 src="https://www.google.com/maps/embed/v1/place?q=springfield+railway+station&key=AIzaSyD7Orx1FRQa9ewpuSP8uOcZU6eEL6m-ziU"></iframe> 
     </section>
   )
